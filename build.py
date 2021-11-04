255c82af629f559ca0ff0ac72e393376410b3eb1
Michal Janočko <michal@janocko.com>
2021-11-04 12:20:50 +0100

    🚚 Move everything out of 'content'

--- build.py
+++ build.py
@@ -1,15 +1,17 @@
 import glob
 import jinja2
 import markdown
+import json
 from pathlib import Path
 
 
 def main():
-    for day in range(1, 2):
+    days = []
+    for day in range(1, 21):
         folder = f"content/{day}"
         pictures = glob.glob(f"{folder}/*.jpg")
 
-        data = Path(f"{folder}/data.json").read_text()
+        data = json.loads(Path(f"{folder}/data.json").read_text())
         post_cs = Path(f"{folder}/post_cs.md").read_text()
         post_en = Path(f"{folder}/post_en.md").read_text()
 
